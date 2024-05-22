9375d60b3e3df2552de02884cb68ef3f863bedb7
juisehytonen2 <juise.hytonen@gmail.com>
2024-05-22 12:10:47 +0300

    Attempt to think about generic listeners, not even close to working

--- Assets/Scripts/EntityComponentListener.cs
+++ Assets/Scripts/EntityComponentListener.cs
@@ -1,23 +1,42 @@
-﻿using Unity.Entities;
+﻿using System;
+using System.Collections.Generic;
+using Components;
+using Unity.Entities;
 
 namespace DefaultNamespace
 {
     public class EntityComponentListener
     {
+        private Dictionary<Type, Delegate> _listeners = new();
         public EntityComponentListener()
         {
             EntityComponentManager.Instance.ComponentAdded += OnComponentAdded;
         }
 
-        public void AddListener()
+        public void AddListener<T>(Func<T> callback)
         {
-            // how to do this....
+            _listeners.Add(typeof(T), callback);
         }
         
-        private void OnComponentAdded(Entity entity, IComponentData component)
+        private void OnComponentAdded(Entity entity, IComponentData componentData)
         {
-            
-            
+            foreach (var kvp in _listeners)
+            {
+                if (kvp.Key == componentData.GetType())
+                {
+                    kvp.Value.DynamicInvoke(entity, componentData);
+                }
+            }
+        }
+
+        private void TestAPI()
+        {
+            AddListener<Position>(OnPositionChanged);
+
+            Position OnPositionChanged<T>(Entity entity, T component)
+            {
+                
+            }
         }
     }
 }
